876c1ee969f83f74a5db7f7428ee7a849bb54bb1
Sean Tronsen <sean.tronsen@gmail.com>
2023-09-24 11:45:46 -0600

    [main] -- added a python dependency check and condition for version echo.

--- lib.bash
+++ lib.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "sourced bash-common-lib version: $(git rev-parse HEAD)"
+echo "sourced bash-common-lib"
+if ls -a $(realpath $(dirname $BASH_SOURCE)) | grep -oE "^\.git$"; then
+	echo "version: $(git rev-parse HEAD)"
+fi
 
 function logger() {
 	echo "[$(date --utc) UTC] $1"
@@ -15,12 +18,23 @@ function error() {
 	exit $2
 }
 
-function dependency_check() {
+function binary_dependency_check() {
 	for arg in "$@"; do
 		if [[ -z "$(which $arg)" ]]; then
 			error "dependency not found: '$arg'"
 		else
 			echo "dependency found: '$arg'"
+		fi
+	done
+}
+
+function python_dependency_check() {
+	binary_dependency_check python3 pip
+	for arg in "$@"; do
+		if pip list | grep -oE "^$arg "; then
+			echo "python dependency found: $arg"
+		else
+			echo "python dependency not found: $arg"
 		fi
 	done
 }
