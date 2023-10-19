8ee53a91a78c62bebfb385ae9b7e87a170edaed6
Sean Tronsen <sean.tronsen@gmail.com>
2023-10-19 14:29:41 -0600

    [main] -- documentation writing and enabled environment variable overrides.

--- lib.bash
+++ lib.bash
@@ -1,8 +1,26 @@
 #!/bin/bash
+################################################################################
+# Author: Sean Tronsen
+# Provide a set of reusable bash functions to be referenced in scripts utilizing
+# this library.
+#
+################################################################################
 
-USER_BIN="$HOME/bin"
-SHELL_CONFIG="$HOME/.bashrc"
+################################################################################
+# Global variables
+#
+# Each may be overridden by a properly specified environment variable.
+################################################################################
+USER_BIN="${USER_BIN:="$HOME/bin"}"
+SHELL_CONFIG="${SHELL_CONFIG:="$HOME/.bashrc"}"
 
+################################################################################
+# Obtain the directory name of the current script file.
+# Globals:
+# 	BASH_SOURCE
+# Outputs:
+# 	a string containing the absolute path to the script file.
+################################################################################
 function script_dir() {
 	echo "$(realpath $(dirname ${BASH_SOURCE[0]}))"
 }
@@ -21,6 +39,17 @@ if ls -a "$LIB_DIR" | grep -oqE "^\.git$"; then
 	)
 fi
 
+################################################################################
+# Ensure that all arguments provided can be located within $PATH
+# Arguments:
+# 	$@: a list of program names
+# Outputs:
+# 	for each argument, output status information regarding whether or not the
+# 	referenced target could be located via the $PATH variable (using `which`)
+# Returns:
+# 	0 if all specified arguments exist on $PATH
+# 	non-zero if any of the specified arguments cannot be located
+################################################################################
 function binary_dependency_check() {
 	for arg in "$@"; do
 		if [[ -z "$(which $arg)" ]]; then
@@ -31,6 +60,17 @@ function binary_dependency_check() {
 	done
 }
 
+################################################################################
+# Ensure that all arguments are valid python packages which can be found by pip.
+# Arguments:
+# 	$@: a list of python package names
+# Outputs:
+# 	for each argument, output status information regarding whether a
+# 	corresponding python package exists.
+# Returns:
+# 	0 if all arguments can be found by pip
+# 	non-zero if any argument could not be located
+################################################################################
 function python_dependency_check() {
 	binary_dependency_check python3 pip
 	for arg in "$@"; do
@@ -42,6 +82,21 @@ function python_dependency_check() {
 	done
 }
 
+################################################################################
+# Configure a local binary directory for the calling user and add the location
+# the the PATH environment variable within the configuration specified '.*rc"
+# file.
+# Globals:
+# 	USER_BIN: a path where the local binary directory will be created, should
+# 	it not exist
+# 	SHELL_CONFIG: path to the user shell configuration script (e.g. `.bashrc`)
+#
+# Outputs:
+# 	status information about each subtask that indicates whether it was not
+# 	necessary or if the action was performed.
+# Returns:
+# 	0
+################################################################################
 function add-user-bin() {
 	if [ ! -d "$USER_BIN" ]; then
 		mkdir -vp "$USER_BIN"
--- logger.bash
+++ logger.bash
@@ -1,17 +1,60 @@
 #!/bin/bash
+################################################################################
+# Author: Sean Tronsen
+# Provide a standard logging API.
+#
+################################################################################
 
+################################################################################
+# Provided a string argument, echo the content prepending with logging
+# information. Serves as the base formatter for higher level logging functions.
+# Arguments:
+# 	$1: string content to be formatted and echoed back to the caller (or defaults
+# 	to stdout)
+# Outputs:
+# 	provided string content with log formatting.
+# Returns:
+# 	0
+################################################################################
 function logger() {
 	echo "[$(hostname)] [$(date -u) UTC] $1"
 }
 
+################################################################################
+# Echo back the provided string content as an 'info' log.
+# Arguments:
+# 	$1: string text content
+# Outputs:
+# 	text content in 'info' log format
+# Returns:
+# 	0
+################################################################################
 function info() {
 	logger "[INFO]: $1"
 }
 
+################################################################################
+# Echo back the provided string content as an 'warning' log.
+# Arguments:
+# 	$1: string text content
+# Outputs:
+# 	text content in 'warning' log format
+# Returns:
+# 	0
+################################################################################
 function warning() {
 	logger "[WARNING]: $1"
 }
 
+################################################################################
+# Echo back the provided string content as an 'error' log.
+# Arguments:
+# 	$1: string text content
+# Outputs:
+# 	text content in 'error' log format
+# Returns:
+# 	0
+################################################################################
 function error() {
 	logger "[ERROR]: $1"
 	exit $2
